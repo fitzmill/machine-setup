f82b60b9c1ad66238c0857244a7b1ac4c7ca5925
Sean Fitzgerald <sfitzmill@gmail.com>
2019-09-09 22:22:13 -0500

    initial commit

---
+++ 01_brew-install.sh
@@ -0,0 +1,34 @@
+# Apps to be installed manually:
+# GroupMe
+
+##################################
+
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+brew install nvm
+brew install pyenv
+brew install yarn
+brew install sl
+brew cask install dotnet
+brew cask install showyedge
+brew cask install visual-studio-code
+brew cask install azure-data-studio
+brew cask install postman
+brew cask install vlc
+brew cask install spotify
+brew cask install discord
+brew cask install gitkraken
+brew cask install qbittorrent
+brew cask install lastpass
+brew cask install multimc
+brew cask install steam
+brew cask install private-internet-access
+brew cask install parsec
+brew cask install adobe-creative-cloud
+brew cask install slack
+brew cask install twitch
+brew cask install microsoft-word
+brew cask install microsoft-excel
+brew cask install microsoft-powerpoint
+brew cask install visual-studio
---
+++ README.md
@@ -0,0 +1,15 @@
+# Machine Setup
+
+A repo for scripts related to setting up new machines with my personal applications/configurations.
+
+## Tasks these scripts don't do (and need to be done manually)
+
+1. SSH Keygen
+2. Clone current repos (and set them up)
+3. Install individual Adobe apps
+4. Log in to various services
+    - Office
+    - Adobe
+    - Spotify
+    - Steam
+5. 
